2c2b7e2f7bd5f630d5050a203a247f460bca4965
Giacomo Sauchelli <gsu@bit4id.com>
2024-12-14 10:10:45 +0100

    first commit

---
+++ debug.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+set -u
+
+echo "Usage: debug|local up|restart|down|logs"
+
+DIR0="$(dirname "$(readlink -f "$0")")"
+echo "$DIR0"
+
+NAME="$(basename "$DIR0")"
+echo "$NAME"
+
+OP1="up"
+OP2="restart"
+OP3="down"
+OP4="logs"
+
+if [ $# -eq 0 ]; then
+  echo "Devi passare almeno un parametro."
+  exit 1
+fi
+
+echo "$1 $2"
+
+cd _deploy/compose/"$1"
+pwd
+
+if [ $2 = "up" ]; then
+  docker compose up -d
+elif [ $2 = "restart" ]; then
+  docker compose restart
+elif [ $2 = "down" ]; then
+  docker compose down
+elif [ $2 = "logs" ]; then
+  docker compose logs -f --tail 1000
+elif [ $2 = "stop" ]; then
+  docker compose stop
+fi
+
+cd ../..

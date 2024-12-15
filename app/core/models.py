d4b2867382931ac265560434e0c2c5e24cf71bed
Giacomo Sauchelli <giacomo.sauchelli@gmail.com>
2024-12-15 20:26:34 +0100

    Add models for Author, Ingridient and Recipe

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=100)
+    execution = models.TextField()
+    execution_time = models.TextField()
+    person_number = models.IntegerField()
+    # noinspection PyTypeChecker
+    author = models.ForeignKey(
+        Author,
+        related_name='author',
+        null=True,
+        on_delete=models.SET_NULL()
+    )
+    quote = models.TextField()
+
+class Ingredient(models.Model):
+    ingredient = models.CharField(max_length=100)
+    amount = models.FloatField()
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.ingredient
+    class Meta:
+        verbose_name_plural = "Ingredients"
+        ordering = ['amount']

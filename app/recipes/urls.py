b4128fee65eaece9deb889c4d29c0e75a8faefbd
Giacomo Sauchelli <giacomo.sauchelli@gmail.com>
2024-12-15 20:30:42 +0100

    Add urls and view for recipe

--- app/recipes/urls.py
+++ app/recipes/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import RecipesView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('recipes/', include('recipes.urls')),
+    path('/', view=RecipesView.as_view(), name='home'),
 ]
